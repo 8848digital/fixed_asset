c6fe47fec2b51c7e8442b009ce458ed7cc907cd8
sibikumarkuppusamy <sibikumark2002@gmail.com>
2024-07-23 18:59:56 +0530

    fix: jv and purchase receipt

--- asset/asset/customizations/journal_entry/doc_events/update_booked_depreciation.py
+++ asset/asset/customizations/journal_entry/doc_events/update_booked_depreciation.py
@@ -2,20 +2,20 @@ import frappe
 
 
 def update_booked_depreciation(self, cancel=0):
-    for d in self.get("accounts"):
-        if (
-            self.voucher_type == "Depreciation Entry"
-            and d.reference_type == "Asset"
-            and d.reference_name
-            and frappe.get_cached_value("Account", d.account, "root_type") == "Expense"
-            and d.debit
-        ):
-            asset = frappe.get_doc("Asset", d.reference_name)
-            for fb_row in asset.get("finance_books"):
-                if fb_row.finance_book == self.finance_book:
-                    if cancel:
-                        fb_row.total_number_of_booked_depreciations -= 1
-                    else:
-                        fb_row.total_number_of_booked_depreciations += 1
-                    fb_row.db_update()
-                    break
+	for d in self.get("accounts"):
+		if (
+			self.voucher_type == "Depreciation Entry"
+			and d.reference_type == "Asset"
+			and d.reference_name
+			and frappe.get_cached_value("Account", d.account, "root_type") == "Expense"
+			and d.debit
+		):
+			asset = frappe.get_doc("Asset", d.reference_name)
+			for fb_row in asset.get("finance_books"):
+				if fb_row.finance_book == self.finance_book:
+					if cancel:
+						fb_row.total_number_of_booked_depreciations -= 1
+					else:
+						fb_row.total_number_of_booked_depreciations += 1
+					fb_row.db_update()
+					break
--- asset/asset/customizations/purchase_receipt/override/update_assets.py
+++ asset/asset/customizations/purchase_receipt/override/update_assets.py
@@ -3,19 +3,19 @@ from frappe.utils import flt
 
 
 def asset_update_assets(self, item, valuation_rate):
-    assets = frappe.db.get_all(
-        "Asset",
-        filters={"purchase_receipt": self.name, "item_code": item.item_code},
-        fields=["name", "asset_quantity"],
-    )
+	assets = frappe.db.get_all(
+		"Asset",
+		filters={"purchase_receipt": self.name, "item_code": item.item_code},
+		fields=["name", "asset_quantity"],
+	)
 
-    for asset in assets:
-        purchase_amount = flt(valuation_rate) * asset.asset_quantity
-        frappe.db.set_value(
-            "Asset",
-            asset.name,
-            {
-                "gross_purchase_amount": purchase_amount,
-                "purchase_amount": purchase_amount,
-            },
-        )
+	for asset in assets:
+		purchase_amount = flt(valuation_rate) * asset.asset_quantity
+		frappe.db.set_value(
+			"Asset",
+			asset.name,
+			{
+				"gross_purchase_amount": purchase_amount,
+				"purchase_amount": purchase_amount,
+			},
+		)
