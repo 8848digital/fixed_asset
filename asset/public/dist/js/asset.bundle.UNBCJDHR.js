bdc2e2a0db9bc0e4229466a1940c86fa6aa8188c
sibikumarkuppusamy <sibikumark2002@gmail.com>
2024-07-29 12:37:52 +0530

    feat: help line

---
+++ asset/public/dist/js/asset.bundle.UNBCJDHR.js
@@ -0,0 +1,19 @@
+(() => {
+  // ../asset/asset/public/js/help_links.js
+  frappe.provide("frappe.help.help_links");
+  var docsUrl = "https://erpnext.com/docs/";
+  frappe.help.help_links["List/Asset"] = [
+    {
+      label: "Managing Fixed Assets",
+      url: docsUrl + "user/manual/en/asset"
+    }
+  ];
+  frappe.help.help_links["List/Asset Category"] = [
+    {
+      label: "Asset Category",
+      url: docsUrl + "user/manual/en/asset-category"
+    }
+  ];
+  console.log("====in public");
+})();
+//# sourceMappingURL=asset.bundle.UNBCJDHR.js.map
---
+++ asset/public/js/asset.bundle.js
@@ -0,0 +1,2 @@
+import "./help_links";
+
