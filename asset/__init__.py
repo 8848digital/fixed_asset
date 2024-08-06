0d7603ce230e8a4dd9cb1c5806cdc5a48045c613
sibikumarkuppusamy <sibikumark2002@gmail.com>
2024-08-06 11:14:32 +0530

    feat: accounts controller

--- asset/__init__.py
+++ asset/__init__.py
@@ -1,9 +1,11 @@
 __version__ = "0.0.1"
 
+from erpnext.controllers.accounts_controller import AccountsController
 from erpnext.controllers.buying_controller import BuyingController
 from erpnext.controllers.stock_controller import StockController
 from erpnext.stock.serial_batch_bundle import SerialBatchBundle
 
+from asset.asset.controllers.accounts_controller.accounts_controller import AssetAccountsController
 from asset.asset.controllers.buying_controller.buying_controller import AssetBuyingController
 from asset.asset.controllers.stock_controller.stock_controller import AssetStockController
 from asset.asset.customizations.serial_and_batch_bundle.serial_batch_bundle import (
@@ -18,3 +20,4 @@ StockController.make_bundle_using_old_serial_batch_fields = (
 	AssetStockController.make_bundle_using_old_serial_batch_fields
 )
 SerialBatchBundle.child_doctype = AssetSerialBatchBundle.child_doctype
+AccountsController.set_missing_item_details = AssetAccountsController.set_missing_item_details
