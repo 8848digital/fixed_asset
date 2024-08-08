629410eeac21c4dd1d3ee99252f9dc523aa9aaa5
sibikumarkuppusamy <sibikumark2002@gmail.com>
2024-08-08 17:15:06 +0530

    fix: bug fix

--- asset/asset/controllers/stock_controller/stock_controller.py
+++ asset/asset/controllers/stock_controller/stock_controller.py
@@ -8,11 +8,9 @@ from asset.asset.controllers.stock_controller.override.make_gl_entries import as
 
 class AssetStockController(StockController):
 	def make_gl_entries(self, gl_entries=None, from_repost=False, via_landed_cost_voucher=False):
-		asset_make_gl_entries(self, gl_entries=None, from_repost=False, via_landed_cost_voucher=False)
+		asset_make_gl_entries(self, gl_entries, from_repost, via_landed_cost_voucher)
 
 	def make_bundle_using_old_serial_batch_fields(
 		self, table_name=None, via_landed_cost_voucher=False
 	):
-		asset_make_bundle_using_old_serial_batch_fields(
-			self, table_name=None, via_landed_cost_voucher=False
-		)
+		asset_make_bundle_using_old_serial_batch_fields(self, table_name, via_landed_cost_voucher)
--- asset/asset/customizations/get_item_details/get_item_details.py
+++ asset/asset/customizations/get_item_details/get_item_details.py
@@ -16,9 +16,8 @@ def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=Tru
 	item = frappe.get_cached_doc("Item", args.item_code)
 	out = get_basic_details(args, item, overwrite_warehouse)
 
-	expense_account = None
-
 	if item.is_fixed_asset:
+		expense_account = None
 		from asset.asset.doctype.asset.asset import get_asset_account, is_cwip_accounting_enabled
 
 		if is_cwip_accounting_enabled(item.asset_category):
@@ -39,15 +38,15 @@ def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=Tru
 				fieldname="fixed_asset_account", item=args.item_code, company=args.company
 			)
 
-	item_defaults = get_item_defaults(item.name, args.company)
-	item_group_defaults = get_item_group_defaults(item.name, args.company)
-	brand_defaults = get_brand_defaults(item.name, args.company)
+		item_defaults = get_item_defaults(item.name, args.company)
+		item_group_defaults = get_item_group_defaults(item.name, args.company)
+		brand_defaults = get_brand_defaults(item.name, args.company)
 
-	out.update(
-		{
-			"expense_account": expense_account
-			or get_default_expense_account(args, item_defaults, item_group_defaults, brand_defaults),
-			"is_fixed_asset": item.is_fixed_asset,
-		}
-	)
+		out.update(
+			{
+				"expense_account": expense_account
+				or get_default_expense_account(args, item_defaults, item_group_defaults, brand_defaults),
+				"is_fixed_asset": item.is_fixed_asset,
+			}
+		)
 	return _get_item_details(args, out, doc, for_validate, overwrite_warehouse)
